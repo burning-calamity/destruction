2bc7c0af980466076682e709103f10db69230f3d
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-28 23:17:47 +0100

    Update and rename install without dependencies.py to install.py

--- destruction/install without dependencies.py
+++
@@ -1,5 +0,0 @@
-def __repr__():
-    return "to install this library without installing dependencies type in cmd:\n\npip install --no-deps git+https://github.com/burning-calamity/destruction.git"
-
-def __str__():
-    return "to install this library without installing dependencies type in cmd:\n\npip install --no-deps git+https://github.com/burning-calamity/destruction.git"
