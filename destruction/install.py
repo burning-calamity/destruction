0ece00af5de1b80167fe6b96b530a433478b9b3e
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-28 23:22:07 +0100

    Update install.py

--- destruction/install.py
+++ destruction/install.py
@@ -1,8 +1,8 @@
 class _install:
-    def __repr__():
+    def __repr__(self):
         return "to install this library without installing dependencies type in cmd:\n\npip install --no-deps git+https://github.com/burning-calamity/destruction.git"
     
-    def __str__():
+    def __str__(self):
         return "to install this library without installing dependencies type in cmd:\n\npip install --no-deps git+https://github.com/burning-calamity/destruction.git"
 
 
