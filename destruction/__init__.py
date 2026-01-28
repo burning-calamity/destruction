d785103dc7059a5d62edb08f8c4d1fc19b3e87a1
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-28 23:22:36 +0100

    Update __init__.py

--- destruction/__init__.py
+++ destruction/__init__.py
@@ -1,4 +1,3 @@
-# destruction/__init__.py
 import pkgutil
 import importlib
 
@@ -9,6 +8,7 @@ __all__ = [m.name for m in pkgutil.iter_modules(__path__)]
 def __getattr__(name):
     if name in __all__:
         module = importlib.import_module(f"{__name__}.{name}")
-        globals()[name] = module
-        return module
+        value = getattr(module, name, module)
+        globals()[name] = value
+        return value
     raise AttributeError(f"module {__name__} has no attribute {name}")
