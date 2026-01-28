46daecbd5787c315d0018456692d9de92d96afae
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-28 22:47:38 +0100

    Add __repr__ and __str__ methods to updater.py

---
+++ destruction/updater.py
@@ -0,0 +1,5 @@
+def __repr__():
+    return "to update this library type in cmd:\n\npip install --upgrade destruction"
+
+def __str__():
+    return "to update this library type in cmd:\n\npip install --upgrade destruction"
