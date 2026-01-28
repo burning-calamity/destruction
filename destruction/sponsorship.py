a3ec88bb727314723d503225795db973336f2f92
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-28 23:21:43 +0100

    Update sponsorship.py

--- destruction/sponsorship.py
+++ destruction/sponsorship.py
@@ -1,8 +1,8 @@
 class _sp:
-    def __repr__():
+    def __repr__(self):
         return "subscribe to: https://www.youtube.com/@theyoutuberyoutubegod"
     
-    def __str__():
+    def __str__(self):
         return "subscribe to: https://www.youtube.com/@theyoutuberyoutubegod"
 
 sponsorship = _sp()
