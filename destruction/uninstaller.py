2d549085d300b8d7df5111034776585d5fe0d223
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-28 22:32:21 +0100

    Implement string representation for uninstaller
    Add __repr__ and __str__ methods for uninstaller.

---
+++ destruction/uninstaller.py
@@ -0,0 +1,5 @@
+def __repr__():
+    return "to uninstall this library type in cmd:\n\npip uninstall destruction"
+
+def __str__():
+    return "to uninstall this library type in cmd:\n\npip uninstall destruction"
