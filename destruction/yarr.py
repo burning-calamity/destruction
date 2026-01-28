84578ab351a94b4e91231b96aa4a9777eedf74cf
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-28 23:20:57 +0100

    Refactor repr and str methods into _yarr class

--- destruction/yarr.py
+++ destruction/yarr.py
@@ -1,5 +1,7 @@
-def __repr__():
-    return "Ahoy mate!"
-
-def __str__():
-    return "Ahoy mate!"
+class _yarr:
+    def __repr__():
+        return "Ahoy mate!"
+    
+    def __str__():
+        return "Ahoy mate!"
+yarr = _yarr()
