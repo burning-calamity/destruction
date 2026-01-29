6c93d84589ee9959dc64df7859c53e0b4599797f
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-29 15:36:23 +0100

    Update autoguess.py

--- destruction/autoguess.py
+++ destruction/autoguess.py
@@ -1,13 +1,20 @@
 from .bruteforce import brute_force_caesar
 from .rot13 import rot13
+from .atbash import atbash
+from .langdetect import detect_language_score
 
-
-def auto_guess(text: str):
+def auto_guess(text: str, top: int = 5):
     guesses = []
 
-    guesses.append(("rot13", rot13(text)))
+    # ROT13
+    guesses.append(("rot13", rot13(text), detect_language_score(rot13(text))))
 
-    for shift, decoded, score in brute_force_caesar(text):
-        guesses.append((f"caesar({shift})", decoded))
+    # Atbash
+    guesses.append(("atbash", atbash(text), detect_language_score(atbash(text))))
 
-    return guesses
+    # Caesar brute force
+    for shift, decoded, score in brute_force_caesar(text, top=top):
+        guesses.append((f"caesar({shift})", decoded, score))
+
+    guesses.sort(key=lambda x: x[2], reverse=True)
+    return guesses[:top]
