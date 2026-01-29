5903a9e168efff94a980b7a909ec8101c7090200
burning-calamity <massimiliano.mosconi@outlook.it>
2026-01-29 15:30:20 +0100

    Update bruteforce.py

--- destruction/bruteforce.py
+++ destruction/bruteforce.py
@@ -1,5 +1,6 @@
 from .caesar import caesar
 from .langdetect import detect_language_score
+from .wordscore import word_score
 
 
 def brute_force_caesar(text: str, top: int = 5):
@@ -11,7 +12,7 @@ def brute_force_caesar(text: str, top: int = 5):
 
     for shift in range(26):
         decoded = caesar(text, shift)
-        score = detect_language_score(decoded)
+        score = detect_language_score(decoded) + word_score(decoded) * 10
         results.append((shift, decoded, score))
 
     results.sort(key=lambda x: x[2], reverse=True)
